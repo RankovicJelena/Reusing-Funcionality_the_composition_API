5f7ace40adde2fdeaa540c9e63fe0fed41aa7e81
Jelena <jelenarankovic0@gmail.com>
2022-11-04 13:27:28 +0100

    custom hooks

--- src/hooks/alert.js
+++ src/hooks/alert.js
@@ -1,1 +1,14 @@
-function useAlert() {}
+import { ref } from 'vue';
+
+export default function useAlert() {
+  const alertIsVisible = ref(false);
+
+  function showAlert() {
+    alertIsVisible.value = true;
+  }
+  function hideAlert() {
+    alertIsVisible.value = false;
+  }
+
+  return [alertIsVisible, showAlert, hideAlert];
+}

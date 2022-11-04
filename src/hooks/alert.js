c08dd31134f1fa63b05ef5a0a9a145a7a359ce2c
Jelena <jelenarankovic0@gmail.com>
2022-11-04 20:23:03 +0100

    more custom composition functions

--- src/hooks/alert.js
+++ src/hooks/alert.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 
-export default function useAlert() {
-  const alertIsVisible = ref(false);
+export default function useAlert(statringVisibility = false) {
+  const alertIsVisible = ref(statringVisibility);
 
   function showAlert() {
     alertIsVisible.value = true;
